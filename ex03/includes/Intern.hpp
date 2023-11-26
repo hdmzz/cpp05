20e6d6cef085f15785cd74d7d6737b6ffa57a494
hugo <hdamitzian@gmail.com>
2023-11-26 02:31:10 +0100

    excetion throw quand intern veut creer un form qui existe pas

--- ex03/includes/Intern.hpp
+++ ex03/includes/Intern.hpp
@@ -6,7 +6,7 @@
 /*   By: hdamitzi <hdamitzi@student.42lyon.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/25 21:01:59 by hdamitzi          #+#    #+#             */
-/*   Updated: 2023/11/25 22:36:09 by hdamitzi         ###   ########.fr       */
+/*   Updated: 2023/11/26 02:30:27 by hdamitzi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,7 +27,12 @@ public:
 
     Intern& operator=(Intern & const rhv);
 
-    AForm*  makeForm(std::string name, std::string target);    
+    AForm*  makeForm(std::string name, std::string target);
+    
+    class FormDoesNotExist : public std::exception {
+        public:
+            virtual const char * what(void) const throw();    
+    }
 };
 
 Intern::Intern(void)
@@ -56,25 +61,39 @@ Intern& Intern::operator=(Intern & const rhv)
     return (*this);
 }
 
-static AForm* createPresidentialPardonForm(std::string target)
+static AForm* createPresidentialPardonForm(std::string const & target)
 {
     return (new PresidentialPardonForm(target));
 }
 
-static AForm* createRobotomyRequestForm(std::string target)
+static AForm* createRobotomyRequestForm(std::string const & target)
 {
     return (new RobotomyRequestForm(target));
 }
 
-static AForm* createShrubberyCreationForm(std::string target)
+static AForm* createShrubberyCreationForm(std::string const & target)
 {
     return (new ShrubberyCreationForm(target));
 }
 
+const char * Intern::FormDoesNotExist::what(void) const throw()
+{
+    return ("The form you want to create does not exist");
+}    
 
 AForm*  Intern::makeForm(std::string name, std::string target)
 {
-    
+    static std::string tab[3] = {"shrubbery request", "presidential pardon", "robotomy request"};
+	AForm *	(*function[3])(std::string const &) = {createPresidentialPardonForm, createRobotomyRequestForm, createShrubberyCreationForm};
+
+    for (int i = 0; i < 3; i++){
+        if (name == tab[i]) {
+            std::cout << "Intern creates " << name << std::endl;
+            return ((*function[i])(target));
+        }
+    }
+    std::cout << "Intern can't create a form named " << name << std::endl;
+    throw (Intern::FormDoesNotExist());
 }
 
 
